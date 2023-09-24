6ea44b0badd8144437c4fb9a66fb2b781a9b9a89
Andrey <bakarbyikov@gmail.com>
2023-09-24 16:14:05 +0500

    File validation added and test moved

---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class BadFile(Exception):
+    """Can't open file"""
--- file_parcer.py
+++ file_parcer.py
@@ -1,10 +1,37 @@
+from io import TextIOWrapper
+
+from exceptions import BadFile
+
+
+def read_size(file: TextIOWrapper) -> int:
+    line = file.readline()
+    try:
+        size = int(line)
+    except ValueError:
+        raise BadFile(f"Некорректный размер матрицы: '{line}'.")
+    else:
+        return size
+
+def read_row(file: TextIOWrapper, n: int, n_parts: int) -> list[int]:
+    line = file.readline()
+    parts = line.split()
+    if len(parts) != n_parts:
+        raise BadFile((f"Некорректная длинна строки {n}: ",
+                        f"Ожидалось {n_parts}, получено {len(parts)}."))
+    result = list()
+    for i, raw in enumerate(parts):
+        try:
+            parsed = int(raw)
+        except ValueError:
+            raise BadFile(f"Некорректный символ {i} строки {n}: {raw}.")
+        else:
+            result.append(parsed)
 
 def read_adjacency(path: str) -> list[list[int]]:
     with open(path) as file:
-        size = int(file.readline())
-        matrix = list()
-        for _ in range(size):
-            row = list(map(int, file.readline().split()))
-            assert len(row) == size
-            matrix.append(row)
+        size = read_size(file)
+        matrix = [read_row(file, i, size) for i in range(size)]
     return matrix
+
+if __name__ == '__main__':
+    print(read_adjacency("examples/empty.txt"))
